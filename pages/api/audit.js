c164ae5c388410e4503d695678c9d018f5444971
pungprakearti <andrew.pungprakearti@gmail.com>
2021-10-19 09:51:37 -0700

    Lighthouse audit working as intended!

--- pages/api/audit.js
+++ pages/api/audit.js
@@ -3,17 +3,14 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 export default function handler(req, res) {
-  let runnerResult
-  let runnerResultMobile
   (async () => {
     const chrome = await chromeLauncher.launch({
       chromeFlags: ['--headless']
     });
 
     const flags = {
-      // logLevel: 'info',
       port: chrome.port,
-      output: 'html',
+      output: 'json',
       onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo'],
       logLevel: 'info'
     }
@@ -33,16 +30,10 @@ export default function handler(req, res) {
       }
     }
 
-    runnerResult = await lighthouse('https://www.rhumbix.com', flags, config);
-    // runnerResultMobile = await lighthouse('https://www.rhumbix.com', flags);
+    const runnerResult = await lighthouse('https://www.rhumbix.com', flags, config);
+    const runnerResultMobile = await lighthouse('https://www.rhumbix.com', flags);
   
     console.log('\n\n\nthis is runnerResult: ', runnerResult?.lhr?.audits, '\n\n\n');
-
-    // `.report` is the HTML report as a string
-    // const reportHtml = runnerResult.report;
-    // const reportHtmlMobile = runnerResultMobile.report;
-    // fs.writeFileSync('lhreport.html', reportHtml);
-    // fs.writeFileSync('lhreportMobile.html', reportHtmlMobile);
   
     // `.lhr` is the Lighthouse Result as a JS object
     console.log('Report is done for', runnerResult?.lhr?.finalUrl);
@@ -50,6 +41,6 @@ export default function handler(req, res) {
   
     await chrome.kill();
 
-    res.status(200).json({ audits: runnerResult?.lhr?.categories })
-  })();
+    res.status(200).json({ audits: runnerResult.lhr })
+  })()
 }
