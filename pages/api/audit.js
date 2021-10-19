17374dd06ed6aed277f8dd2dda229b91bd4d122a
pungprakearti <andrew.pungprakearti@gmail.com>
2021-10-19 15:37:22 -0700

    Parse most data from api

--- pages/api/audit.js
+++ pages/api/audit.js
@@ -12,9 +12,9 @@ export default function handler(req, res) {
       output: 'json',
       onlyCategories: [
         'performance',
-        'accessibility',
-        'best-practices',
-        'seo'
+        // 'accessibility',
+        // 'best-practices',
+        // 'seo'
       ],
       logLevel: 'info'
     }
@@ -37,10 +37,15 @@ export default function handler(req, res) {
     let runnerResult
     let runnerResultMobile
     try {
-      runnerResult = await lighthouse('www.google.com', flags, config);
-      runnerResultMobile = await lighthouse('www.google.com', flags);
+      runnerResult = await lighthouse('https://www.google.com', flags, config);
+      runnerResultMobile = await lighthouse('https://www.google.com', flags);
     } catch(err) {
-      return res.status(500).json({ error: err?.code })
+      if(err.code === 'INVALID_URL') {
+        return res.status(500).json({ error: `${err?.code} - example: https://www.google.com` })
+      } else {
+        return res.status(500).json({ error: err?.code })
+      }
+
     }
   
     console.log('\n\n\nthis is runnerResult: ', runnerResult?.lhr?.audits, '\n\n\n');
@@ -51,7 +56,70 @@ export default function handler(req, res) {
   
     await chrome.kill();
 
-    return res.status(200).json({ audits: runnerResult.lhr })
+    // Parse data
+    /*
+    url, <--- get from body
+    type, <---
+    performance,
+    accessibility,
+    bestpractices,
+    seo,
+    fcp,
+    si,
+    lcp,
+    tti,
+    tbt,
+    cls,
+    date,
+    */
+    const date = Date.now()
+    const accessibility = runnerResult?.lhr?.categories?.accessibility?.score * 100
+    const bestpractices = runnerResult?.lhr?.categories['best-practices']?.score * 100
+    const cls = runnerResult?.lhr?.audits['cumulative-layout-shift']?.score * 100
+    const fcp = runnerResult?.lhr?.audits['first-contentful-paint']?.score * 100
+    const lcp = runnerResult?.lhr?.audits['largest-contentful-paint']?.score * 100
+    const performance = runnerResult?.lhr?.categories?.performance?.score * 100
+    const seo = runnerResult?.lhr?.categories?.seo?.score * 100
+    const si = runnerResult?.lhr?.audits['speed-index']?.score * 100
+    const tbt = runnerResult?.lhr?.audits['total-blocking-time']?.score * 100
+    const tti = runnerResult?.lhr?.audits?.interactive?.score * 100
+
+    const accessibility_mobile = runnerResultMobile?.lhr?.categories?.accessibility?.score * 100
+    const bestpractices_mobile = runnerResultMobile?.lhr?.categories['best-practices']?.score * 100
+    const cls_mobile = runnerResultMobile?.lhr?.audits['cumulative-layout-shift']?.score * 100
+    const fcp_mobile = runnerResultMobile?.lhr?.audits['first-contentful-paint']?.score * 100
+    const lcp_mobile = runnerResultMobile?.lhr?.audits['largest-contentful-paint']?.score * 100
+    const performance_mobile = runnerResultMobile?.lhr?.categories?.performance?.score * 100
+    const seo_mobile = runnerResultMobile?.lhr?.categories?.seo?.score * 100
+    const si_mobile = runnerResultMobile?.lhr?.audits['speed-index']?.score * 100
+    const tbt_mobile = runnerResultMobile?.lhr?.audits['total-blocking-time']?.score * 100
+    const tti_mobile = runnerResultMobile?.lhr?.audits?.interactive?.score * 100
+
+    const audits = {
+      date,
+      accessibility,
+      bestpractices,
+      cls,
+      fcp,
+      lcp,
+      performance,
+      seo,
+      si,
+      tbt,
+      tti,
+      accessibility_mobile,
+      bestpractices_mobile,
+      cls_mobile,
+      fcp_mobile,
+      lcp_mobile,
+      performance_mobile,
+      seo_mobile,
+      si_mobile,
+      tbt_mobile,
+      tti_mobile,
+    }
+
+    return res.status(200).json({ audits: audits })
   }
 
   if(req.method === 'POST') {
